beed90385ff65bea2d9518d66630e131e1ca6c1c
madvirus <madvirus@madvirus.net>
2015-01-05 18:26:55 +0900

    초기 기반 작업 중

---
+++ src/main/java/net/madvirus/eval/command/domain/evaluation/Evaluation.java
@@ -0,0 +1,6 @@
+package net.madvirus.eval.command.domain.evaluation;
+
+import org.axonframework.eventsourcing.annotation.AbstractAnnotatedAggregateRoot;
+
+public class Evaluation extends AbstractAnnotatedAggregateRoot<String> {
+}
